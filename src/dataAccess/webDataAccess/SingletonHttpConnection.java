97b609588e8d73875d7d639bc169658d02670c72
Joachim LEJEUNE <etu44137@henallux.be>
2022-04-22 09:23:32 +0200

    starting Singleton creation Https request

---
+++ src/dataAccess/webDataAccess/SingletonHttpConnection.java
@@ -0,0 +1,12 @@
+package dataAccess.webDataAccess;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.net.URL;
+
+public class SingletonHttpConnection {
+    private HttpsURLConnection connection;
+
+    public HttpsURLConnection getConnection(){
+        URL url =  ""
+    }
+}
